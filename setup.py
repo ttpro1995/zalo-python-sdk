c4602487bec5f1348de5d54425056da845e05f41
ntkduy123 <ntkduy123@gmail.com>
2017-11-20 08:42:27 +0700

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup (
-    name = 'ZaloPythonSDK',
-    packages = ['ZaloPythonSDK'],
-    version = '1.0',
+    name = 'zalo-python-sdk',
+    packages = find_packages(),
+    version = '1.11',
     description = 'Python SDK for Zalo API',
     author = 'Nguyen Thuy Khanh Duy',
     author_email = 'duyntk@vng.com.vn',
     url = 'https://github.com/ntkduy123/zalo-python-sdk',
-    download_url = '',
+    download_url = 'https://github.com/ntkduy123/zalo-python-sdk/archive/0.1.tar.gz',
     keywords = [],
     classifiers = []
 )
